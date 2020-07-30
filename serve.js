26ecd2f14c1ebc631f204b22c00e4bdfea765213
MartinezYonathan <j-ma@hotmail.es>
2020-07-29 23:30:48 -0500

    commit 3

--- serve.js
+++ serve.js
@@ -6,11 +6,10 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/Incidencias'));
- 
-app.get('*', function(req,res) {
+app.use(express.static(__dirname + '/dist/incidencias'));
+app.get('/*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/Incidencias/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/incidencias/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
