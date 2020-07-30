b9019eba41fcee699327ad523cb1476c2a8221f1
MartinezYonathan <j-ma@hotmail.es>
2020-07-30 00:11:21 -0500

    commit 6

--- serve.js
+++ serve.js
@@ -1,15 +1,22 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+
 const app = express();
- 
+
 // Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/incidencias'));
+app.use(express.static('./dist/incidencias'));
+
 app.get('/*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/incidencias/index.html'));
+
+  if(req.url === '/') {
+    res.sendFile(path.join(__dirname,'/dist/incidencias/index.html'));
+  }
+  else {
+    res.sendFile(path.join(__dirname, '/dist/pi-project' + req.url))
+  }
+
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
