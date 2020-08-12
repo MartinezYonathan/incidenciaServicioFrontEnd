439eb104261e3df6bfe39f23cf3553e0ebc43821
MartinezYonathan <j-ma@hotmail.es>
2020-08-12 02:44:31 -0500

    commit 3 server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/incidencias'));
 
 app.get('*', function(req, res) {
     // Replace the '/dist/<to_your_project_name>/index.html'
-    res.sendFile(path.join(__dirname + '/incidencias/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/incidencias/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
