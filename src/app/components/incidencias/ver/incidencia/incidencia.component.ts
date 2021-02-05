da046bc72508a8440ab61b7b20c2d12f8b089019
MartinezYonathan <j-ma@hotmail.es>
2021-02-04 22:56:08 -0600

    se movio el componente de evidencias y seguimiento

--- src/app/components/incidencias/ver/incidencia/incidencia.component.ts
+++ src/app/components/incidencias/ver/incidencia/incidencia.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Expediente } from 'src/app/model/expediente-model';
 import { Incidencia } from 'src/app/model/incidencia-model';
 import { TokenStorageService } from 'src/app/services/token-storage.service';
 
@@ -14,10 +15,14 @@ export class IncidenciaComponent implements OnInit {
     private tokenService: TokenStorageService
   ) {}
   @Input() incidencias: any[];
+  expedientes: Expediente;
   username: string;
 
   ngOnInit(): void {
     this.username = this.tokenService.getUser();
+    this.expedientes = JSON.parse(window.localStorage.getItem("EXPEDIENTE"));
+    this.incidencias = this.expedientes.incidencias;
+
   }
 
   goToIncidencia(incidencia: Incidencia): void {
