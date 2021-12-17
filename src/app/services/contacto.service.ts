fe4b55579268f49104b73116f38846fb379a8ae5
Yonathan Martinez <33378318+MartinezYonathan@users.noreply.github.com>
2021-12-16 18:39:11 -0600

    Add files via upload

---
+++ src/app/services/contacto.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.prod';
+import { Contacto } from 'src/app/model/contacto-model';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactoService {
+  API_URL = environment.apiUrl;
+
+  constructor(private http: HttpClient) { 
+    console.log('Servicio Contacto Funcionando');
+  }
+
+
+  contactosGet(): Observable<Contacto[]> {
+    return this.http.get<Contacto[]>(this.API_URL + 'contacto/');
+  }
+
+  contactosPost(contacto): Observable<any> {
+    return this.http.post(this.API_URL + 'contacto/', {
+      nombreCompleto: contacto.nombreCompleto,
+      celular:contacto.celular,
+      email: contacto.email,
+      tipoAlarma:contacto.tipoAlarma,
+      parentesco: contacto.parentesco
+    }, httpOptions);
+  }
+
+  deleteComentario(id:number): Observable<any> {
+    return this.http.delete<any>(this.API_URL + 'contacto/' + id);
+  }
+}
